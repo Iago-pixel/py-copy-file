db1973ed8b0399e7fdca8899c0ae4da4f6c94c0c
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-09-16 09:30:59 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ def copy_file(command: str) -> None:
     if len(command_parts) != 3 or command_parts[0] != "cp":
         return None
 
-    file_name = command_parts[1]
-    new_file_name = command_parts[2]
+    source_file_name = command_parts[1]
+    target_file_name = command_parts[2]
 
-    if file_name == new_file_name:
+    if source_file_name == target_file_name:
         return None
 
     try:
-        with open(file_name, "r") as file_in:
-            with open(new_file_name, "a") as file_out:
+        with open(source_file_name, "r") as file_in:
+            with open(target_file_name, "w") as file_out:
                 file_out.write(file_in.read())
     except FileNotFoundError:
         return None
